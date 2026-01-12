ae56c3ff15a8f9eaf941d01d211d699533e86760
Carlo Matic <carlomatic@MacBookPro.fritz.box>
2026-01-12 08:53:39 +0100

    feat: Replace Twitter logo with X logo and update URL

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,12 @@
 import { motion } from 'framer-motion';
-import { Facebook, Instagram, Linkedin, Mail, Twitter } from 'lucide-react';
+import { Facebook, Instagram, Linkedin, Mail } from 'lucide-react';
 import { useLanguage } from '../hooks/useLanguage';
+
+const XLogo = ({ size = 20 }: { size?: number }) => (
+    <svg width={size} height={size} viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
+    </svg>
+);
 
 export function Footer() {
     const currentYear = new Date().getFullYear();
@@ -29,7 +35,7 @@ export function Footer() {
                         <SocialLink href="https://linkedin.com/in/carlomatic" icon={Linkedin} label="LinkedIn" />
                         <SocialLink href="https://www.instagram.com/carlomatic/" icon={Instagram} label="Instagram" />
                         <SocialLink href="https://www.facebook.com/carlo.matic78" icon={Facebook} label="Facebook" />
-                        <SocialLink href="https://twitter.com/carlomatic" icon={Twitter} label="Twitter" />
+                        <SocialLink href="https://x.com/carlomatic" icon={XLogo} label="X (Twitter)" />
                         <SocialLink
                             onClick={() => {
                                 const user = 'cm';
